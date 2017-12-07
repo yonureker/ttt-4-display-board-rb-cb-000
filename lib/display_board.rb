9b50f0e4166c12dd2a672ac8238f2ca266dd8d51
Onur Eker <yonureker@gmail.com>
2017-12-07 01:19:50 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,10 +5,10 @@ def display_board(board)
   pipe = "|"
   row_lines = "-----------"
 
-  puts "#{board[0]}" + pipe + "#{board[1]}" + pipe + "#{board[2]}"
+  puts " #{board[0]} " + pipe + " #{board[1]} " + pipe + " #{board[2]} "
   puts row_lines
-  puts "#{board[3]}" + pipe + "#{board[4]}" + pipe + "#{board[5]}"
+  puts " #{board[3]} " + pipe + " #{board[4]} " + pipe + " #{board[5]} "
   puts row_lines
-  puts "#{board[6]}" + pipe + "#{board[7]}" + pipe + "#{board[8]}"
+  puts " #{board[6]} " + pipe + " #{board[7]} " + pipe + " #{board[8]} "
 
 end
